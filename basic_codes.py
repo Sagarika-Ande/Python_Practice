246ad5181962c9552b274cadfcf26c97c2a17a59
sagarika <andesagarika@gmail.com>
2025-03-12 15:31:17 +0530

    added python file

---
+++ basic_codes.py
@@ -0,0 +1,15 @@
+# num1=int(input ("enter first number"))
+# num2=int(input("enter second number"))
+# sum=num1+num2
+# # print(f"sum:{num1}+{num2}={sum}")
+#
+# print("sum:",num1,"+",num2,"=",sum)
+#
+# import random
+# print("random num:"{random.randint(1,100)}")
+
+
+def add(a,b):
+    c = a + b
+    return print(c)
+add(5,2)
